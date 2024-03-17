ce2ed8377ce45aeec12c7c8c809d1a09ca82dd25
root <root@LAPTOP-3SFQ684H>
2024-03-17 15:49:00 +0530

    Created a lambda resource and an API endpoint

--- acp_sample_app/acp_sample_app_stack.py
+++ acp_sample_app/acp_sample_app_stack.py
@@ -1,19 +1,62 @@
+#!/usr/bin/env python3
+
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda as _lambda,
+    aws_apigateway as _apigateway,
 )
 from constructs import Construct
+import os
+
 
 class AcpSampleAppStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        hello_world_lambda = self.build_lambda()
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AcpSampleAppQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        acp_app_apigateway = self.build_apigateway()
+
+        test_api = acp_app_apigateway.root.add_resource('test', default_cors_preflight_options=_apigateway.CorsOptions(
+            allow_methods=['GET', 'OPTIONS'],
+            allow_origins=_apigateway.Cors.ALL_ORIGINS)
+        )
+
+        test_api.add_method(
+            'GET',
+            _apigateway.LambdaIntegration(
+                hello_world_lambda,
+                proxy=False,
+                integration_responses=[
+                    _apigateway.IntegrationResponse(
+                        status_code="200",
+                        response_parameters={
+                            'method.response.header.Access-Control-Allow-Origin': "'*'"
+                        }
+                    )
+                ]
+            ),
+            method_responses=[
+                _apigateway.MethodResponse(
+                    status_code="200",
+                    response_parameters={
+                        'method.response.header.Access-Control-Allow-Origin': True
+                    }
+                )
+            ]
+        )
+
+    def build_lambda(self) -> _lambda.Function:
+        path = os.path
+        asset_path = path.join(path.realpath(
+            __file__).split(path.sep)[-2], 'lambda')
+        hello_world_lambda = _lambda.Function(self, 'helloWorldLambda',
+                                              handler='helloWorld.handler',
+                                              runtime=_lambda.Runtime.PYTHON_3_12,
+                                              code=_lambda.Code.from_asset(asset_path))
+        return hello_world_lambda
+
+    def build_apigateway(self) -> _apigateway.RestApi:
+        return _apigateway.RestApi(self, 'helloWorldApigateway',
+                                   rest_api_name='helloWorldApigateway')
